b6eb2864d430bcee4442915446bfbe53ef934a05
Umak26 <122770863+Umak26@users.noreply.github.com>
2023-07-18 20:01:19 +0530

    Update views.py

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,13 @@
-from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render
 
 from .models import Question
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
-
-
-# Leave the rest of the views (detail, results, vote) unchanged
+# ...
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
